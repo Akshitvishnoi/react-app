7b8e8e5ea9305b871e76aaff9c15abe0cbd7aafa
Akshit Vishnoi <shankydubey455@gmail.com>
2024-07-23 14:32:52 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,13 +17,13 @@ const Hero = () => {
           </p>
           <ReactTyped
             className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['Digital Makerting', 'Web Development', 'Ethical Hacking']}
-            typeSpeed={120}
-            backSpeed={140}
+            strings={['Software Development', 'Web Development', 'IoS Development']}
+            typeSpeed={110}
+            backSpeed={110}
             loop
           />
         </div>
-        <button className='bg-[black] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Get Started</button>
+        <button className='bg-[black] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Let's Started</button>
       </div>
     </div>
   );
