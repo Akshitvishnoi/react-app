a64f4696540bbf9e28f9bfbce6f4386349376d79
Akshit Vishnoi <shankydubey455@gmail.com>
2024-07-23 14:42:31 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,14 +12,14 @@ const Footer = () => {
       <div>
         <h1 className='w-full text-3xl font-bold text-black'>WsCube Tech.</h1>
         <p className='py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit ullam iste repellat consequatur libero reiciendis, blanditiis accusantium.</p>
-        <div className='flex justify-between md:w-[75%] my-6'>
-          <button className='hover:scale-105 '>
+   <button>     
+          <div className='flex justify-between md:w-[75%] my-6 hover:scale-105 duration-500'> 
           <FaFacebookSquare size={30} />
           <FaInstagram size={30} />
           <FaTwitterSquare size={30} />
-          <FaGithubSquare size={30} />
-          </button>
+          <FaGithubSquare size={30} />          
         </div>
+   </button>
       </div>
       <div className='lg:col-span-2 flex justify-between mt-6'>
         <div>
